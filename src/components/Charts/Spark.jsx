345107d91ed1e5d17851a210ad7d3b79de161aff
glenn-pickett <glenndpickett@gmail.com>
2023-11-09 12:42:19 -0500

    Fixed Spark error

--- src/components/Charts/Spark.jsx
+++ src/components/Charts/Spark.jsx
@@ -1,11 +1,15 @@
-import { SparklineComponent, Inject, SparklineTooltip } from "@syncfusion/ej2-react-charts"
+import { SparklineComponent, Inject, SparklineTooltip } from "@syncfusion/ej2-react-charts";
 
 
 const Spark = ({id, height, width, color, data, type, currentColor}) => {
     return (
-        <SparklineComponent id={id} height={height} width={width} lineWidth={1} valueType="Numeric" fill={color} border={{ color: currentColor, width: 2}} dataSource={data} xName='x' yName='y' type={type} tooltipSettings={{ visible: true, format: '${x} : data ${y}', trackLineSettings: {visible: true}}}>
-            <Inject services={[SparklineTooltip]} />
-        </SparklineComponent>
+        <>
+            {type && (
+                <SparklineComponent id={id} height={height} width={width} lineWidth={1} valueType="Numeric" fill={color} border={{ color: currentColor, width: 2 }} dataSource={data} xName='x' yName='y' type={type} tooltipSettings={{ visible: true, format: '${x} : data ${y}', trackLineSettings: { visible: true } }}>
+                    <Inject services={[SparklineTooltip]} />
+                </SparklineComponent>
+            )}
+        </>
     )
 }
 
--- src/pages/Needs.jsx
+++ src/pages/Needs.jsx
@@ -4,57 +4,7 @@ import { KanbanComponent, ColumnsDirective, ColumnDirective } from '@syncfusion/
 import { useEffect, useState } from 'react';
 
 const Needs = () => {
-    /*
-    const [state, setstate] = useState();
-
-    let allWithClass = null;
-    //let allWithClass = [];
-    useEffect(() => {
-        allWithClass = document.getElementById('Needs');
-
-        let theHole = null;
-
-        if(allWithClass.firstElementChild && allWithClass.firstElementChild.nextSibling){
-            theHole = allWithClass.firstElementChild.nextSibling.firstChild.firstChild.lastChild.firstChild.firstChild;
-        }
-        //.firstElementChild.nextSibling.firstChild.firstChild.lastChild.firstChild.firstChild
-        
-        let newI = allWithClass.querySelectorAll('tr');
-        const fetchData = async () => {
-            function sleep(ms) {
-                return new Promise(resolve => setTimeout(resolve, ms));
-            }
-        }
-
-        setstate(theHole);
-        console.log('allWithClass called', allWithClass, newI, state, theHole)
-        addColor(state);
-    }, [allWithClass, state]);
-
-    /*
-    useEffect(() => {
-        // declare the async data fetching function
-        const fetchData = async () => {
-            let needArray = null;
-            function sleep(ms) {
-                return new Promise(resolve => setTimeout(resolve, ms));
-            }
-
-            needArray = await Array.from(document.querySelectorAll('.e-kanban .e-kanban-table .e-header-cells')) && Array.from(document.querySelectorAll('.e-kanban .e-kanban-table .e-header-cells')) != undefined ? Array.from(document.querySelectorAll('.e-kanban .e-kanban-table .e-header-cells')) : [];
-            console.log('needArray',needArray, 'fetchData',fetchData, 'state',state);
-            //setstate(needArray);
-
-        };
-
-        const result = fetchData()
-            // make sure to catch any error
-            .catch(console.error);;
-
-        // what will be logged to the console?
-        console.log('Result', result);
-    }, [])*/
-
-    const styles = {
+     const styles = {
         0: [
             'bisque'
         ],
@@ -69,6 +19,7 @@ const Needs = () => {
         ],
     }
 
+    /*
     const addColor = (i) => {
         for (let key in i) {
             if(i[key])
@@ -76,7 +27,7 @@ const Needs = () => {
             i[key].class = 'red';
             //console.log('addColor', i[key], styles[key], key);
         }
-    }
+    }*/
 
     return (
         <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
